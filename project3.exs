14117f21ac9f151d177d8b4a2279616ebc0e2bd1
Mohit Garg <mohitgarg@ufl.edu>
2019-10-18 13:42:41 -0400

    small change

--- project3.exs
+++ project3.exs
@@ -1,4 +1,3 @@
-arguments = Enum.to_list(System.argv())
-numNodes = String.to_integer(Enum.at(arguments, 0), 10)
-numRequests = String.to_integer(Enum.at(arguments, 1), 10)
+numNodes = String.to_integer(Enum.at(System.argv(),0))
+numRequests = String.to_integer(Enum.at(System.argv(),1))
 Proj3.main([numNodes, numRequests])
