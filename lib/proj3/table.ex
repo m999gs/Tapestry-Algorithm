8a8ce9b9b5faa240a28d30a8092150cfc8bdef38
Mohit Garg <mohitgarg@ufl.edu>
2019-10-23 12:04:10 -0400

    complete routing table

--- lib/proj3/table.ex
+++ lib/proj3/table.ex
@@ -7,7 +7,7 @@ defmodule Proj3.Table do
             level= longest_prefix(hashID,x,0,0)
            q= Enum.reduce(0..level,acc,fn y, acc2->
                 temp= String.at(x,y)
-                {_, rlevel} = Map.fetch(routingTable, y)
+                {_, rlevel} = Map.fetch(acc, y)
                 x = Map.put(rlevel, temp, x)
                 acc2 = Map.put(acc2,y,x)
             end)
